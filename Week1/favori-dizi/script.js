495eddc42755092e0a9388ffcede444318873e4f
burakugras <burakugras67@gmail.com>
2025-02-23 21:20:21 +0300

    add characters section

--- Week1/favori-dizi/script.js
+++ Week1/favori-dizi/script.js
@@ -1,12 +1,20 @@
-const navElm = document.querySelector('.nav');
-const hamburgerElm = document.querySelector('.hamburger');
+const navElm = document.querySelector(".nav");
+const hamburgerElm = document.querySelector(".hamburger");
 
-hamburgerElm.addEventListener('click', () => {
-    navElm.classList.toggle('nav--open');
-    hamburgerElm.classList.toggle('hamburger--open');
+hamburgerElm.addEventListener("click", () => {
+  navElm.classList.toggle("nav--open");
+  hamburgerElm.classList.toggle("hamburger--open");
 });
 
-navElm.addEventListener('click', () => {
-    navElm.classList.remove('nav--open');
-    hamburgerElm.classList.remove('hamburger--open');
+navElm.addEventListener("click", () => {
+  navElm.classList.remove("nav--open");
+  hamburgerElm.classList.remove("hamburger--open");
 });
+
+
+document.querySelector('.slider').addEventListener('wheel', function(e) {
+    if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
+      e.preventDefault();
+    }
+  }, { passive: false });
+  
