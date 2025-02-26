f0fc3e053f413cf51e2b8c28ba71ce40708df39a
burakugras <burakugras67@gmail.com>
2025-02-26 14:12:46 +0300

    update exercise 2

--- Week2/exercise-2/script.js
+++ Week2/exercise-2/script.js
@@ -3,7 +3,7 @@ const longestCollatz = (num) => {
   while (num > 1) {
     if (num % 2 === 0) {
       num /= 2;
-    } else if (num % 2 !== 0) {
+    } else {
       num = 3 * num + 1;
     }
     counter++;
