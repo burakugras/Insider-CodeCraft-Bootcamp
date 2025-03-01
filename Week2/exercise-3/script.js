06a74169960433cfed0fb880a053ffebdf8dd217
burakugras <burakugras67@gmail.com>
2025-03-01 03:17:39 +0300

    add exercise 3

---
+++ Week2/exercise-3/script.js
@@ -0,0 +1,26 @@
+let countdownInterval;
+let countdownValue=0;
+
+const startCount = () => {
+  countdownValue = Number(document.getElementById("countdown-input").value);
+
+  if (isNaN(countdownValue) || countdownValue <= 0 || countdownValue > 25) {
+    alert("Geçersiz bir sayı girdiniz!");
+    return;
+  }
+
+  countdownInterval = setInterval(() => {
+    document.getElementById("display").innerText = countdownValue;
+    countdownValue--;
+
+    if (countdownValue < 0) {
+      clearInterval(countdownInterval);
+      document.getElementById("sub-display").innerText = "Süre doldu!";
+    }
+  }, 1000);
+};
+
+const reset = () => {
+  clearInterval(countdownInterval);
+  document.getElementById("display").innerText="Sıfırlandı!"
+};
